e52e1651481137eb11c60bef115f9831f62b028a
Cintrian <velegoj2@yandex.ru>
2022-09-27 21:11:04 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 3};
-        for (int i = array1.length-1; i >= 0; i--) {
-            if (i == 0) {
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] %2 != 0){
+                array1[i] = array1[i] + 1;
+            }
+            if (i == array1.length-1) {
                 System.out.print(array1[i]);
             } else {
                 System.out.print(array1[i] + ", ");
-            }
-        }
-        System.out.println();
-        double[] array2 = {1.57, 7.654, 9.986};
-        for (int i = array2.length-1; i >= 0; i--) {
-            {
-                if (i == 0) {
-                    System.out.print(array2[i]);
-                } else {
-                    System.out.print(array2[i] + ", ");
-                }
-            }
-        }
-        System.out.println();
-        double[] array3 = {10, 2, 1421, 23, 412.4, 543, 534.124, 8};
-        for (int i = array3.length-1; i >=0; i--) {
-            if (i == 0) {
-                System.out.print(array3[i]);
-            } else {
-                System.out.print(array3[i] + ", ");
             }
         }
     }
